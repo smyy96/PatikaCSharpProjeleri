d75c6bd9cff5e4b32a412903eda8f7057bdba471
smyy <smyy.cskn@hotmail.com>
2022-08-26 13:34:57 +0300

    Add project files.

---
+++ OrtalamaHesaplama/FibonacciOrt/FibonacciOrt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FibonacciOrt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fibNum = 0;
+            Console.Write("Sayı giriniz: ");
+            fibNum = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            double seri = 1;
+            double seri2= 2;
+            double tut = 0;
+            for (int i = 1; i <= (fibNum-2); i++) // serinin ilk iki degerini almadıgım için döngüyü 2 azalttım
+            {
+                tut = seri2;
+                seri2 += seri;
+                seri = tut;
+
+                sum += tut;
+            }
+
+            double ort =(1 + 1 + sum) / fibNum; // 1 + 1 fibonacci serisinin ilk iki degeri
+            Console.WriteLine("Fibonacci Ortalama: "+ ort);
+        }
+    }
+}
